844dac277e3f40f355aa1b0d853b356dca044a92
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:53:28 -0500

    feat: add service request and response interfaces.

---
+++ src/app/core/application/dto/services/service-request.dto.ts
@@ -0,0 +1,5 @@
+export interface IServiceRequest {
+    name:        string;
+    description: string;
+    price:       number;
+}
---
+++ src/app/core/application/dto/services/service-response.dto.ts
@@ -0,0 +1,7 @@
+export interface IServiceResponse {
+    created_at:  string;
+    description: string;
+    id:          number;
+    name:        string;
+    price:       string;
+}
