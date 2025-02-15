4f27d0e3a732a1075af6d32194edca39e9a3408c
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:15:47 -0500

    feat: add Icons component.

---
+++ src/UI/atoms/icons/Icons.tsx
@@ -0,0 +1,29 @@
+import { LuCalendarFold } from "react-icons/lu";
+import { MdOutlineAccessTime } from "react-icons/md";
+import { TbRefresh } from "react-icons/tb";
+import { MdNavigateNext } from "react-icons/md";
+import { MdNavigateBefore } from "react-icons/md";
+import { RiScissors2Fill } from "react-icons/ri";
+import { GiBeard } from "react-icons/gi";
+import { GiComb } from "react-icons/gi";
+import { GiRazor } from "react-icons/gi";
+import { PiSprayBottleFill } from "react-icons/pi";
+import { IoIosCloseCircle } from "react-icons/io";
+import { MdEventAvailable } from "react-icons/md";
+
+
+
+export const Icons = {
+    calendar: <LuCalendarFold />,
+    time: <MdOutlineAccessTime />,
+    status: <TbRefresh />,
+    next: <MdNavigateNext />,
+    before: <MdNavigateBefore />,
+    scissors: <RiScissors2Fill />,
+    beard: <GiBeard />,
+    comb: <GiComb />,
+    razor: <GiRazor />,
+    spray: <PiSprayBottleFill />,
+    close: <IoIosCloseCircle />,
+    event: <MdEventAvailable />
+}
