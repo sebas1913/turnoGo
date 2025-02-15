bf29c08407f5bc5ea3432a505ea89a2c4c92d2d3
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:52:51 -0500

    feat: add UserTemplate component.

---
+++ src/UI/template/userTemplate/userTemplate.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from 'react';
+import { IAppointmentResponse } from "@/app/core/application/dto/appointment/appointment-response.dto";
+import styles from "./template.module.scss";
+import UserGreeting from "@/UI/molecules/userGreeting/UserGreeting";
+import TableAppointments from "@/UI/organisms/tableAppointments/TableAppointments";
+import Modal from '@/UI/organisms/modal/Modal';
+import AppointmentForm from '@/UI/organisms/appoitnmentForm/AppointmentForm';
+
+interface IProps {
+    dataResponse: IAppointmentResponse[];
+}
+
+const UserTemplate: React.FC<IProps> = ({ dataResponse }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className={styles.sidebar}>
+                    <UserGreeting openModal={openModal} />
+                </div>
+                <div className={styles.content}>
+                    <TableAppointments dataResponse={dataResponse} />
+                </div>
+            </div>
+
+            <Modal isVisible={isModalOpen} onClose={closeModal}>
+                <AppointmentForm onClose={closeModal}/>
+            </Modal>
+        </>
+    );
+};
+
+export default UserTemplate;
+
