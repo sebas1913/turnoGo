d35a2956ac2d8ccab975cc8d883f198358d4c43c
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:14:38 -0500

    feat: add appointment request and response interfaces.

---
+++ src/app/core/application/dto/appointment/appointment-request.dto.ts
@@ -0,0 +1,5 @@
+export interface IAppointmentRequest {
+    service_id: number;
+    date:       string;
+    time:       string;
+}
---
+++ src/app/core/application/dto/appointment/appointment-response.dto.ts
@@ -0,0 +1,9 @@
+export interface IAppointmentResponse {
+    created_at: Date;
+    date: Date;
+    id: number;
+    service_id: number;
+    status: string;
+    time: string;
+    user_id: number;
+}
