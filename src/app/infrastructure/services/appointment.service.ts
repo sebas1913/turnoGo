aff5819d3159fcff2e78f76c8e278bc8fded182b
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:55:12 -0500

    feat: add AppointmentService for handling service requests.

---
+++ src/app/infrastructure/services/appointment.service.ts
@@ -0,0 +1,31 @@
+import { IAppointmentResponse } from "@/app/core/application/dto/appointment/appointment-response.dto";
+import { HttpClient } from "../utils/httpClient";
+import { IAppointmentRequest } from "@/app/core/application/dto/appointment/appointment-request.dto";
+
+export class AppointmentService {
+    private httpClient: HttpClient;
+
+    constructor() {
+        this.httpClient = new HttpClient();
+    }
+
+    async find(): Promise<IAppointmentResponse> {
+        try {
+            const response = await this.httpClient.get<IAppointmentResponse>(`/appointments`);
+            return response;
+        } catch (error) {
+            console.error("Error fetching appointments:", error);
+            throw error;
+        }
+    }
+
+    async create(body: IAppointmentRequest) {
+        try {
+            const newService = await this.httpClient.post<IAppointmentResponse, IAppointmentRequest>('/appointments', body);
+            return newService;
+        } catch (error) {
+            console.log("Error al crear la cita:", error);
+            throw error;
+        }
+    }
+}
