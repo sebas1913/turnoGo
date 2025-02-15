d9a864fd3c762e44856f916667bb841254038675
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-14 19:49:20 -0500

    feat: add AppointmentForm component.

---
+++ src/UI/organisms/appoitnmentForm/AppointmentForm.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import * as yup from "yup";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState, useEffect } from "react";
+import { IAppointmentRequest } from "@/app/core/application/dto/appointment/appointment-request.dto";
+import { FormField } from "@/UI/molecules/formField/formField";
+import { SelectField } from "@/UI/molecules/selectField/SelectField";
+import Button from "@/UI/atoms/button/Button";
+import Title from "@/UI/atoms/title/Title";
+import styles from "./appointment.module.scss";
+
+const appointmentSchema = yup.object().shape({
+    service_id: yup.number().required("Debes seleccionar un servicio"),
+    date: yup.string().required("Debes seleccionar una fecha"),
+    time: yup.string().required("Debes seleccionar una hora"),
+});
+
+interface IService {
+    id: number;
+    name: string;
+}
+
+interface AppointmentFormProps {
+    onClose: () => void;
+}
+
+const AppointmentForm: React.FC<AppointmentFormProps> = ({ onClose }) => {
+    const {
+        handleSubmit,
+        control,
+        formState: { errors }
+    } = useForm<IAppointmentRequest>({
+        mode: "onChange",
+        reValidateMode: "onChange",
+        resolver: yupResolver(appointmentSchema)
+    });
+
+    const [services, setServices] = useState<IService[]>([]);
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            try {
+                const response = await fetch("/api/services");
+                const data = await response.json();
+                setServices(data);
+            } catch (error) {
+                console.error("Error al cargar servicios", error);
+            }
+        };
+
+        fetchServices();
+    }, []);
+
+    const handleAppointment = async (data: IAppointmentRequest) => {
+        try {
+            const response = await fetch("/api/appointments", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            onClose();
+
+    
+            if (!response.ok) {
+                const errorData: { msg: string } = await response.json();
+    
+                if (errorData.msg) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error",
+                        text: errorData.msg,
+                    });
+                }
+    
+                return;
+            }
+    
+            await Swal.fire({
+                icon: 'success',
+                title: '¡Cita creada!',
+                text: 'Tu cita ha sido agendada con éxito.',
+                confirmButtonText: 'Aceptar',
+                confirmButtonColor: '#fc9137',
+            });
+        
+        } catch (error) {
+            console.error(error);
+            Swal.fire({
+                icon: "error",
+                title: "Error",
+                text: "Hubo un problema al agendar tu cita.",
+                confirmButtonColor:'#fc9137'
+            });
+        }
+    };
+    
+
+    return (
+        <form onSubmit={handleSubmit(handleAppointment)}>
+            <Title className={styles.title} level={2}>Agenda tu cita</Title>
+            <SelectField
+                control={control}
+                name="service_id"
+                label="Selecciona un servicio"
+                options={services.map((service) => ({
+                    value: service.id,
+                    label: service.name
+                }))}
+                error={errors.service_id}
+            />
+
+            <FormField
+                control={control}
+                type="date"
+                label="Selecciona la fecha"
+                name="date"
+                error={errors.date}
+                placeholder="Ingresa la fecha de tu cita"
+                
+            />
+
+            <FormField
+                control={control}
+                type="time"
+                label="Selecciona la hora"
+                name="time"
+                error={errors.time}
+                placeholder="Ingresa la hora de tu cita"
+            />
+
+            <Button variant="primary" type="submit">Agendar Cita</Button>
+        </form>
+    );
+};
+
+export default AppointmentForm;
