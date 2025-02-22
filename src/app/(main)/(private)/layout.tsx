d0a7e6fde1fe940d64b03163bba07afd1d9bb118
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-22 14:43:14 -0500

    refactor: private layout.

--- src/app/(main)/(private)/layout.tsx
+++ src/app/(main)/(private)/layout.tsx
@@ -1,43 +1,42 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter, usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Spinner from "@/UI/atoms/spinner/Spinner";
 import Navbar from "@/UI/organisms/navbar/Navbar";
 import Button from "@/UI/atoms/button/Button";
+import SecondaryNav from "@/UI/organisms/SecondaryNav/SecondaryNav";
 
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
     const { data: session, status } = useSession();
     const router = useRouter();
     const pathname = usePathname();
-    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         if (status === "loading") return;
-
-        if (!session) {
+    
+        if (status === "unauthenticated") {
             router.replace("/login");
             return;
         }
-
-        if (session.user.role === "ADMIN" && pathname !== "/dashboard") {
-            router.replace("/dashboard");
-            return;
-        }
-
-        if (session.user.role !== "ADMIN" && pathname !== "/users") {
+    
+        const { role } = session?.user || {};
+    
+        if (role === "ADMIN" && !pathname.startsWith("/dashboard")) {
+            router.replace("/dashboard/appointments");
+        } else if (role !== "ADMIN" && pathname !== "/users") {
             router.replace("/users");
-            return;
         }
-
-        setIsLoading(false);
-    }, [session, status, pathname, router]);
+    }, [status, session, pathname, router]);
+    
+    
+    
 
     const handleSignOut = async () => {
         await signOut({ callbackUrl: "/" });
     };
 
-    if (status === "loading" || isLoading) {
+    if (status === "loading") {
         return <Spinner />;
     }
 
@@ -46,6 +45,9 @@ export default function PrivateLayout({ children }: { children: React.ReactNode 
             <Navbar>
                 <Button variant="primary" onClick={handleSignOut}>Cerrar sesión</Button>
             </Navbar>
+
+            {session?.user.role === "ADMIN" && pathname.startsWith("/dashboard") && <SecondaryNav />}
+            
             {children}
         </>
     );
