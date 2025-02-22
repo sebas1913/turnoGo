4effea9c27c66b03cb24c83d3fe4789e75568070
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-22 14:53:08 -0500

    feat: add POST in API route Services.

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { ServicesService } from "@/app/infrastructure/services/services.service";
+import { IServiceRequest } from "@/app/core/application/dto/services/service-request.dto";
 
 const services = new ServicesService();
 
@@ -15,3 +16,15 @@ export async function GET() {
     }
 }
 
+
+export async function POST(request: Request) {
+    try {
+        const body: IServiceRequest = await request.json();
+        const response = await services.create(body);
+
+        return NextResponse.json(response, { status: 200 });
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Unknown error";
+        return NextResponse.json({ message: `Error: ${errorMessage}` }, { status: 500 });
+    }
+}
