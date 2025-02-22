1fe4a99846fa6c46fb36ece18fb26b6ba6e02091
sebas1913 <sebasosornicastrillon@gmail.com>
2025-02-22 11:26:09 -0500

    feat: add new variant to Button component.

--- src/UI/atoms/button/Button.tsx
+++ src/UI/atoms/button/Button.tsx
@@ -1,7 +1,7 @@
 import styles from './button.module.scss';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
-    variant: 'primary' | 'secondary' | 'banner' | 'transparent' | 'pagination' | 'modal';
+    variant: 'primary' | 'secondary' | 'banner' | 'transparent' | 'pagination' | 'modal' | 'canceled' | 'confirmed';
 }
 
 const Button: React.FC<ButtonProps> = ({ variant, children, ...props }) => {
